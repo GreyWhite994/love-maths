0e44455c9931d1899ffeb22a446f54b7af67a925
Graham White <greywhite994@gmail.com>
2023-05-06 20:00:17 +0000

    Added favicon and final tidy ups for user

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,10 +12,20 @@ document.addEventListener("DOMContentLoaded", function() {
         })
     }
 
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    })
+
     rungame("addition");
 })
 
 function runGame(gameType) {
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
+
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
 
